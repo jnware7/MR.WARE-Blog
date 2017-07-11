b0c3927a1d77b871eccaf36001a8fcfe487a91a1
jnware7 <jnware7@gmail.com>
2017-07-11 16:17:38 -0700

    third change form email submission

--- public/js/jqBootstrapValidation.js
+++ public/js/jqBootstrapValidation.js
@@ -64,7 +64,7 @@
 
           if (warningsFound) {
             if (settings.options.preventSubmit) {
-              e.preventDefault();
+              // e.preventDefault();
             }
             $form.addClass("error");
             if ($.isFunction(settings.options.submitError)) {
@@ -478,7 +478,7 @@
                 // How many errors did we find?
                 if (settings.options.semanticallyStrict && errorsFound.length === 1) {
                   // Only one? Being strict? Just output it.
-                  $helpBlock.html(errorsFound[0] + 
+                  $helpBlock.html(errorsFound[0] +
                     ( settings.options.prependExistingHelpBlock ? $helpBlock.data("original-contents") : "" ));
                 } else {
                   // Multiple? Being sloppy? Glue them together into an UL.
